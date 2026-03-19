6d771fefdd37a60ee7f104bbef998ccb4a2a98a1
Joe Politz <joe.politz@gmail.com>
2026-03-19 12:43:24 -0700

    put the Point starter code back to just comments

--- examples/point/Point.java
+++ examples/point/Point.java
@@ -1,8 +1,8 @@
-class Point {
-  double x;
-  double y;
-  Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
-}
+// From writeup:
+
+// Design a class Point for representing 2d cartesian points with two methods:
+
+//  distFrom0 that returns the distance from the origin to this point
+
+// Write a method dist that takes another point and returns the distance between them
+
